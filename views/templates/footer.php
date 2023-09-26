81fe510c259cff5a5ae8149642da6f78b5bd6bc9
Arturo Hernandez Garza <arturo_hdzg@hotmail.com>
2023-09-26 15:36:38 -0600

    Update footer.php

---
+++ views/templates/footer.php
@@ -0,0 +1,71 @@
+<footer class="footer">
+
+  <div class="footer__grid">
+    <div class="footer__contenido">
+      <h3 class="footer__logo">&#60;DevWebCamp /></h3>
+      <p class="footer__texto">
+        DevWebCamp es una conferencia para desarrolladores de todos los niveles,
+        se lleva a cabo de forma presencial y en línea.
+      </p>
+    </div>
+
+    <nav class="menu-redes">
+      <a
+        class="menu-redes__enlace"
+        rel="noopener noreferrer"
+        target="_blank"
+        href="https://www.facebook.com/arturo.hernandezgarza"
+      >
+        <span class="menu-redes__ocultar">Facebook</span>
+      </a>
+      <a
+        class="menu-redes__enlace"
+        rel="noopener noreferrer"
+        target="_blank"
+        href="https://twitter.com/arturo_hdzg"
+      >
+        <span class="menu-redes__ocultar">Twitter</span>
+      </a>
+      <a
+        class="menu-redes__enlace"
+        rel="noopener noreferrer"
+        target="_blank"
+        href="https://www.youtube.com/@ArturoHDZG"
+      >
+        <span class="menu-redes__ocultar">YouTube</span>
+      </a>
+      <a
+        class="menu-redes__enlace"
+        rel="noopener noreferrer"
+        target="_blank"
+        href="https://www.instagram.com/arturohdzgarza"
+      >
+        <span class="menu-redes__ocultar">Instagram</span>
+      </a>
+      <a
+        class="menu-redes__enlace"
+        rel="noopener noreferrer"
+        target="_blank"
+        href="https://www.tiktok.com/@arturo.hdz"
+      >
+        <span class="menu-redes__ocultar">TikTok</span>
+      </a>
+      <a
+        class="menu-redes__enlace"
+        rel="noopener noreferrer"
+        target="_blank"
+        href="https://arturohdzg.github.io/ArturoHDZG/"
+      >
+        <span class="menu-redes__ocultar">GitHub</span>
+      </a>
+    </nav>
+
+  </div>
+
+  <p class="footer__copyright">DevWebCamp <?php echo date('Y'); ?>&copy;
+    <span class="footer__copyright--regular">
+       - Designed by Arturo Hernández Garza
+    </span>
+  </p>
+
+</footer>
