d54c3466b209532b17a03dcb4a89f167e43a0471
Arturo Hernandez Garza <arturo_hdzg@hotmail.com>
2023-09-25 18:00:29 -0600

    Update header.php

---
+++ views/templates/header.php
@@ -0,0 +1,17 @@
+<header class="header">
+  <div class="header__contenedor">
+    <nav class="header__navegacion">
+      <a href="/registro" class="header__enlace">registro</a>
+      <a href="/login" class="header__enlace">Iniciar Sesión</a>
+    </nav>
+
+    <div class="header__contenido">
+      <a href="/">
+        <h1 class="header__logo">&#60;DevWebCamp /></h1>
+      </a>
+      <p class="header__texto">Diciembre 9-10 - 2023</p>
+      <p class="header__texto header__texto--modalidad">En Línea - Presencial</p>
+      <a href="/registro" class="header__boton">Comprar Pase</a>
+    </div>
+  </div>
+</header>
