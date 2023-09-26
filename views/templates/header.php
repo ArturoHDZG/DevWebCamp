8cbe327f3e724a9bfa04a11534af6e8027ce7db6
Arturo Hernandez Garza <arturo_hdzg@hotmail.com>
2023-09-26 00:11:22 -0600

    Update header.php, _barra.scss, _navegacion.scss

--- views/templates/header.php
+++ views/templates/header.php
@@ -1,6 +1,7 @@
 <header class="header">
   <div class="header__contenedor">
-    <nav class="header__navegacion">
+
+    <nav class="header__navegacion" aria-label="Barra de Navegación">
       <a href="/registro" class="header__enlace">registro</a>
       <a href="/login" class="header__enlace">Iniciar Sesión</a>
     </nav>
@@ -13,5 +14,23 @@
       <p class="header__texto header__texto--modalidad">En Línea - Presencial</p>
       <a href="/registro" class="header__boton">Comprar Pase</a>
     </div>
+
   </div>
 </header>
+
+<div class="barra">
+  <div class="barra__contenido">
+
+    <a href="/">
+      <h2 class="barra__logo">&#60;DevWebCamp /></h2>
+    </a>
+
+    <nav class="navegacion" aria-label="Barra de Navegación">
+      <a href="/devwebcamp" class="navegacion__enlace">Evento</a>
+      <a href="/paquetes" class="navegacion__enlace">Paquetes</a>
+      <a href="/workshops-conferencias" class="navegacion__enlace">Workshops / Conferencias</a>
+      <a href="/registro" class="navegacion__enlace">Comprar Pase</a>
+    </nav>
+
+  </div>
+</div>
