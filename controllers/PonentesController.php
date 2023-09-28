d6bae9e5a7095850432343af49c735ccd3828ac9
Arturo Hernandez Garza <arturo_hdzg@hotmail.com>
2023-09-28 15:04:35 -0600

    Update public/index.php.php, PonentesController.php, ponentes/index.php, _dashboard.scss Created ponentes/crear.php

--- controllers/PonentesController.php
+++ controllers/PonentesController.php
@@ -12,4 +12,11 @@ class PonentesController
       'titulo' => 'Ponentes / Conferencistas'
     ]);
   }
+
+  public static function crear(Router $router)
+  {
+    $router->render('admin/ponentes/crear', [
+      'titulo' => 'Registrar Ponente'
+    ]);
+  }
 }
---
+++ views/admin/ponentes/crear.php
@@ -0,0 +1,8 @@
+<h2 class="dashboard__heading"><?php echo $titulo ?></h2>
+
+<div class="dashboard__contenedor-boton">
+  <a href="/admin/ponentes" class="dashboard__boton">
+    <i class="fa-solid fa-circle-arrow-left"></i>
+    Regresar
+  </a>
+</div>
