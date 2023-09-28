80ae7d834185f33eb068559d4ee555385a2b4a13
Arturo Hernandez Garza <arturo_hdzg@hotmail.com>
2023-09-28 01:06:23 -0600

    Update _dashboard.scss, admin-sidebar.php

--- views/templates/admin-sidebar.php
+++ views/templates/admin-sidebar.php
@@ -1,24 +1,24 @@
-<aside class="dashboard_sidebar">
+<aside class="dashboard__sidebar">
 
   <nav class="dashboard__menu">
     <a href="/admin/dashboard" class="dashboard__enlace">
-      <i class="fa-solid fa-house"></i>
+      <i class="fa-solid fa-house dashboard__icono"></i>
       <span class="dashboard__menu-texto">Inicio</span>
     </a>
     <a href="/admin/ponentes" class="dashboard__enlace">
-      <i class="fa-solid fa-microphone"></i>
+      <i class="fa-solid fa-microphone dashboard__icono"></i>
       <span class="dashboard__menu-texto">Ponentes</span>
     </a>
     <a href="/admin/eventos" class="dashboard__enlace">
-      <i class="fa-solid fa-calendar"></i>
+      <i class="fa-solid fa-calendar dashboard__icono"></i>
       <span class="dashboard__menu-texto">Eventos</span>
     </a>
     <a href="/admin/registrados" class="dashboard__enlace">
-      <i class="fa-solid fa-users"></i>
+      <i class="fa-solid fa-users dashboard__icono"></i>
       <span class="dashboard__menu-texto">Registrados</span>
     </a>
     <a href="/admin/registrados" class="dashboard__enlace">
-      <i class="fa-solid fa-gift"></i>
+      <i class="fa-solid fa-gift dashboard__icono"></i>
       <span class="dashboard__menu-texto">Regalos</span>
     </a>
   </nav>
