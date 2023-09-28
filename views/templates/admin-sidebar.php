11585c8e4af6cafa34c1813b1bba3cca81bb5996
Arturo Hernandez Garza <arturo_hdzg@hotmail.com>
2023-09-27 23:57:26 -0600

    Update _dashboard.scss, admin-sidebar.php

---
+++ views/templates/admin-sidebar.php
@@ -0,0 +1,26 @@
+<aside class="dashboard_sidebar">
+
+  <nav class="dashboard__menu">
+    <a href="/admin/dashboard" class="dashboard__enlace">
+      <i class="fa-solid fa-house"></i>
+      <span class="dashboard__menu-texto">Inicio</span>
+    </a>
+    <a href="/admin/ponentes" class="dashboard__enlace">
+      <i class="fa-solid fa-microphone"></i>
+      <span class="dashboard__menu-texto">Ponentes</span>
+    </a>
+    <a href="/admin/eventos" class="dashboard__enlace">
+      <i class="fa-solid fa-calendar"></i>
+      <span class="dashboard__menu-texto">Eventos</span>
+    </a>
+    <a href="/admin/registrados" class="dashboard__enlace">
+      <i class="fa-solid fa-users"></i>
+      <span class="dashboard__menu-texto">Registrados</span>
+    </a>
+    <a href="/admin/registrados" class="dashboard__enlace">
+      <i class="fa-solid fa-gift"></i>
+      <span class="dashboard__menu-texto">Regalos</span>
+    </a>
+  </nav>
+
+</aside>
