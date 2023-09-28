27d5ee206900c71bd342f76b784a5d0a58ab583a
Arturo Hernandez Garza <arturo_hdzg@hotmail.com>
2023-09-28 14:46:36 -0600

    Update funciones.php, admin-sidebar.php, _dashboard.scss

--- includes/funciones.php
+++ includes/funciones.php
@@ -11,3 +11,7 @@ function s($html) : string {
   $s = htmlspecialchars($html);
   return $s;
 }
+
+function paginaActual($path) : bool {
+  return str_contains($_SERVER['PATH_INFO'], $path) ? true : false;
+}
