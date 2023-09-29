9682858bb83dfa19d4e17b8d1c789254b61ededf
Arturo Hernandez Garza <arturo_hdzg@hotmail.com>
2023-09-28 20:08:21 -0600

    Update _formulario.scss, ponentes/formulario.php

--- views/admin/ponentes/formulario.php
+++ views/admin/ponentes/formulario.php
@@ -69,11 +69,105 @@
     <input
       id="tags-input"
       type="text"
-      class="formulario__input formulario__input",
+      class="formulario__input",
       placeholder="Ej. Node.js, UI/UX, Git, Java"
     >
 
     <div id="tags" class="formulario__listado"></div>
     <input type="hidden" name="tags" value="<?php echo $ponente->tags ?? ''; ?>">
+  </div>
+</fieldset>
+
+<fieldset class="formulario__fieldset">
+  <legend class="formulario__legend">Redes Sociales</legend>
+
+  <div class="formulario__campo">
+    <div class="formulario__contenedor-icono">
+      <div class="formulario__icono">
+        <i class="fa-brands fa-facebook"></i>
+      </div>
+    <input
+      name="redes[facebook]"
+      type="text"
+      class="formulario__input--sociales"
+      placeholder="Facebook"
+      value="<?php echo $ponente->facebook ?? ''; ?>"
+    >
+    </div>
+  </div>
+
+  <div class="formulario__campo">
+    <div class="formulario__contenedor-icono">
+      <div class="formulario__icono">
+        <i class="fa-brands fa-twitter"></i>
+      </div>
+    <input
+      name="redes[twitter]"
+      type="text"
+      class="formulario__input--sociales"
+      placeholder="Twitter"
+      value="<?php echo $ponente->twitter ?? ''; ?>"
+    >
+    </div>
+  </div>
+
+  <div class="formulario__campo">
+    <div class="formulario__contenedor-icono">
+      <div class="formulario__icono">
+        <i class="fa-brands fa-youtube"></i>
+      </div>
+    <input
+      name="redes[youtube]"
+      type="text"
+      class="formulario__input--sociales"
+      placeholder="YouTube"
+      value="<?php echo $ponente->youtube ?? ''; ?>"
+    >
+    </div>
+  </div>
+
+  <div class="formulario__campo">
+    <div class="formulario__contenedor-icono">
+      <div class="formulario__icono">
+        <i class="fa-brands fa-instagram"></i>
+      </div>
+    <input
+      name="redes[instagram]"
+      type="text"
+      class="formulario__input--sociales"
+      placeholder="Instagram"
+      value="<?php echo $ponente->instagram ?? ''; ?>"
+    >
+    </div>
+  </div>
+
+  <div class="formulario__campo">
+    <div class="formulario__contenedor-icono">
+      <div class="formulario__icono">
+        <i class="fa-brands fa-tiktok"></i>
+      </div>
+    <input
+      name="redes[tiktok]"
+      type="text"
+      class="formulario__input--sociales"
+      placeholder="TikTok"
+      value="<?php echo $ponente->tiktok ?? ''; ?>"
+    >
+    </div>
+  </div>
+
+  <div class="formulario__campo">
+    <div class="formulario__contenedor-icono">
+      <div class="formulario__icono">
+        <i class="fa-brands fa-github"></i>
+      </div>
+    <input
+      name="redes[github]"
+      type="text"
+      class="formulario__input--sociales"
+      placeholder="GitHub"
+      value="<?php echo $ponente->github ?? ''; ?>"
+    >
+    </div>
   </div>
 </fieldset>
