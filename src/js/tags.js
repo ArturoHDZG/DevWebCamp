4dbb96f1bacd955b5564f52d5d1085ba5d9bd43c
Arturo Hernandez Garza <arturo_hdzg@hotmail.com>
2023-09-28 22:01:29 -0600

    Updated tags.js

--- src/js/tags.js
+++ src/js/tags.js
@@ -1,9 +1,11 @@
-'use strict';
+ 'use strict';
 
 (function () {
   const tagsInput = document.querySelector('#tags-input');
 
   if (tagsInput) {
+    const tagsDiv = document.querySelector('#tags');
+    const tagsInputHidden = document.querySelector('[name="tags"]');
     let tags = [];
 
     // Escuchar cambios en el input
@@ -18,8 +20,32 @@
         e.preventDefault();
         tags = [ ...tags, e.target.value.trim() ];
         tagsInput.value = '';
-        console.log(tags);
+
+        mostrarTags();
       }
+    }
+
+    function mostrarTags() {
+      tagsDiv.textContent = '';
+      tags.forEach(tag => {
+        const etiqueta = document.createElement('LI');
+        etiqueta.classList.add('formulario__tag');
+        etiqueta.textContent = tag;
+        etiqueta.ondblclick = eliminarTag;
+        tagsDiv.appendChild(etiqueta);
+
+        actualizarInputHidden();
+      });
+    }
+
+    function eliminarTag(e) {
+      e.target.remove();
+      tags = tags.filter(tag => tag !== e.target.textContent);
+      console.log(tags);
+    }
+
+    function actualizarInputHidden() {
+      tagsInputHidden.value = tags.toString();
     }
   }
 })();
