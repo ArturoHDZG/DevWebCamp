3767083d9edca81b6380f0adee6cfd33843e0ed7
Arturo Hernandez Garza <arturo_hdzg@hotmail.com>
2023-10-05 00:30:47 -0600

    Updated: base/_mixins.scss, base/_tipografia.scss Created: paginas/devwebcamp.php, paginas/_devwebcamp.scss

---
+++ views/paginas/devwebcamp.php
@@ -0,0 +1,32 @@
+<main class="devwebcamp">
+  <h2 class="devwebcamp__heading"><?php echo $titulo; ?></h2>
+  <p class="devwebcamp__descripcion">Conoce la Conferencia más Importante de Hispanoamérica</p>
+
+  <div class="devwebcamp__grid">
+    <div class="devwebcamp__imagen">
+      <picture>
+        <source srcset="build/img/sobre_devwebcamp.avif" type="image/avif">
+        <source srcset="build/img/sobre_devwebcamp.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/sobre_devwebcamp.jpg" alt="Ejemplo de Evento">
+      </picture>
+    </div>
+
+    <div class="devwebcamp__contenido">
+      <p class="devwebcamp__texto">
+        DevWebCamp es el evento más importante de Hispanoamérica para los
+        apasionados del desarrollo web. En este encuentro anual, podrás
+        aprender de los mejores ponentes de diferentes países, que compartirán
+        sus conocimientos y experiencias sobre las últimas tendencias y
+        tecnologías del sector.
+      </p>
+      <br>
+      <p class="devwebcamp__texto">
+        Además, podrás participar en talleres prácticos, networking y
+        actividades lúdicas, que harán de este evento una experiencia
+        única e inolvidable. Si quieres formar parte de la comunidad
+        DevWebCamp, no te pierdas la próxima edición, que se celebrará
+        en Cuernavaca, México, del 9 al 10 de diciembre de 2023. ¡Te esperamos!
+      </p>
+    </div>
+  </div>
+</main>
