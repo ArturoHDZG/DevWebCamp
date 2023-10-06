bedb8ca4334f85b79773fbf3a4844fa675cad9a0
Arturo Hernandez Garza <arturo_hdzg@hotmail.com>
2023-10-05 18:42:26 -0600

    Created: paginas/conferencias.php, paginas/conferencias.scss

---
+++ views/paginas/conferencias.php
@@ -0,0 +1,22 @@
+<main class="agenda">
+  <h2 class="agenda__heading"><?php echo $titulo; ?></h2>
+  <p class="agenda__descripcion">
+    Conferencias y Talleres Dictados por Expertos en Desarrollo Web
+  </p>
+
+  <div class="eventos">
+    <h3 class="eventos__heading">&lt;Conferencias /></h3>
+    <p class="eventos__fecha">Sábado, 9 de diciembre</p>
+    <div class="eventos__listado"></div>
+    <p class="eventos__fecha">Domingo, 10 de diciembre</p>
+    <div class="eventos__listado"></div>
+  </div>
+
+  <div class="eventos eventos--workshops">
+    <h3 class="eventos__heading">&lt;Workshops /></h3>
+    <p class="eventos__fecha">Sábado, 9 de diciembre</p>
+    <div class="eventos__listado"></div>
+    <p class="eventos__fecha">Domingo, 10 de diciembre</p>
+    <div class="eventos__listado"></div>
+  </div>
+</main>
