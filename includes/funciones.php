3aa42f543e78e045d2e4266e2f4aa5a01ebb2dc5
Arturo Hernandez Garza <arturo_hdzg@hotmail.com>
2023-10-06 20:34:12 -0600

    Updated: includes/funciones.php, templates/header.php, base/_mixins.scss, header/_header.scss, header/_navegacion.scss

--- includes/funciones.php
+++ includes/funciones.php
@@ -17,11 +17,17 @@ function paginaActual($path) : bool {
 }
 
 function isAuth() : bool {
-  session_start();
+
+  if (!isset($_SESSION)) {
+    session_start();
+  }
   return isset($_SESSION['nombre']) && !empty($_SESSION);
 }
 
 function isAdmin() : bool {
-  session_start();
+
+  if (!isset($_SESSION)) {
+    session_start();
+  }
   return isset($_SESSION['admin']) && !empty($_SESSION['admin']);
 }
--- views/templates/header.php
+++ views/templates/header.php
@@ -2,8 +2,20 @@
   <div class="header__contenedor">
 
     <nav class="header__navegacion" aria-label="Barra de Navegación">
-      <a href="/registro" class="header__enlace">registro</a>
-      <a href="/login" class="header__enlace">Iniciar Sesión</a>
+      <?php if (isAuth()) : ?>
+        <a
+          href="<?php echo isAdmin() ? '/admin/dashboard' : '/finalizar-registro'; ?>"
+          class="header__enlace"
+        >
+          Administrar
+        </a>
+        <form class="header__form" method="POST" action="/logout">
+          <input class="header__submit" type="submit" value="Cerrar Sesión">
+        </form>
+      <?php else : ?>
+        <a href="/registro" class="header__enlace">Registro</a>
+        <a href="/login" class="header__enlace">Iniciar Sesión</a>
+      <?php endif; ?>
     </nav>
 
     <div class="header__contenido">
@@ -26,10 +38,38 @@
     </a>
 
     <nav class="navegacion" aria-label="Barra de Navegación">
-      <a href="/devwebcamp" class="navegacion__enlace">Evento</a>
-      <a href="/paquetes" class="navegacion__enlace">Paquetes</a>
-      <a href="/workshops-conferencias" class="navegacion__enlace">Workshops / Conferencias</a>
-      <a href="/registro" class="navegacion__enlace">Comprar Pase</a>
+      <a
+        href="/devwebcamp"
+        class="
+          navegacion__enlace <?php echo paginaActual('/devwebcamp') ? 'navegacion__enlace--actual' : ''; ?>
+        "
+      >
+        Evento
+      </a>
+      <a
+        href="/paquetes"
+        class="
+          navegacion__enlace <?php echo paginaActual('/paquetes') ? 'navegacion__enlace--actual' : ''; ?>
+        "
+      >
+        Paquetes
+      </a>
+      <a
+        href="/workshops-conferencias"
+        class="
+          navegacion__enlace <?php echo paginaActual('/workshops-conferencias') ? 'navegacion__enlace--actual' : ''; ?>
+        "
+      >
+        Workshops / Conferencias
+      </a>
+      <a
+        href="/registro"
+        class="
+          navegacion__enlace <?php echo paginaActual('/registro') ? 'navegacion__enlace--actual' : ''; ?>
+        "
+      >
+        Comprar Pase
+      </a>
     </nav>
 
   </div>
