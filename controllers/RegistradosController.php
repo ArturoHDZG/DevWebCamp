ba9a4dc52d05244c163cc9a1bbb42c8d862a30a2
Arturo Hernandez Garza <arturo_hdzg@hotmail.com>
2023-10-10 21:34:20 -0600

    Updated: controllers/RegistradosController.php, registrados/index.php

--- controllers/RegistradosController.php
+++ controllers/RegistradosController.php
@@ -3,13 +3,49 @@
 namespace Controllers;
 
 use MVC\Router;
+use Model\Registro;
+use Classes\Paginacion;
+use Model\Paquete;
+use Model\Usuario;
 
 class RegistradosController
 {
   public static function index(Router $router)
   {
+    if (!isAdmin()) {
+      header('Location: /login');
+    }
+
+    $paginaActual = $_GET['page'];
+    $paginaActual = filter_var($paginaActual, FILTER_VALIDATE_INT);
+
+    if (!$paginaActual || $paginaActual < 1) {
+      header('Location: /admin/registrados?page=1');
+    }
+
+    $registrosPorPagina = 50;
+    $totalRegistros = Registro::total();
+    $paginacion = new Paginacion(
+      $paginaActual,
+      $registrosPorPagina,
+      $totalRegistros
+    );
+
+    if ($paginacion->totalPaginas() < $paginaActual) {
+      header('Location: /admin/registrados?page=1');
+    }
+
+    $registros = Registro::paginar($registrosPorPagina, $paginacion->offset());
+
+    foreach ($registros as $registro) {
+      $registro->usuario = Usuario::find($registro->usuario_id);
+      $registro->paquete = Paquete::find($registro->paquete_id);
+    }
+
     $router->render('admin/registrados/index', [
-      'titulo' => 'Usuarios Registrados'
+      'titulo' => 'Usuarios Registrados',
+      'registros' => $registros,
+      'paginacion' => $paginacion->paginacion()
     ]);
   }
 }
--- views/admin/registrados/index.php
+++ views/admin/registrados/index.php
@@ -1,1 +1,44 @@
 <h2 class="dashboard__heading"><?php echo $titulo ?></h2>
+
+<div class="dashboard__contenedor">
+  <?php if (!empty($registros)): ?>
+    <table class="table" aria-describedby="Listado Usuarios">
+      <thead class="table__thead">
+        <tr>
+          <th class="table__th" scope="col">Nombre</th>
+          <th class="table__th" scope="col">Email</th>
+          <th class="table__th" scope="col">Plan</th>
+          <th class="table__th" scope="col">Boleto</th>
+        </tr>
+      </thead>
+
+      <tbody class="table__tbody">
+        <?php foreach ($registros as $registro) : ?>
+          <tr class="table__tr">
+
+            <td class="table__td">
+              <?php echo $registro->usuario->nombre . ' ' . $registro->usuario->apellido; ?>
+            </td>
+
+            <td class="table__td">
+              <?php echo $registro->usuario->email; ?>
+            </td>
+
+            <td class="table__td">
+              <?php echo $registro->paquete->nombre; ?>
+            </td>
+
+            <td class="table__td">
+              <?php echo 'No.', ' ', $registro->token; ?>
+            </td>
+
+          </tr>
+        <?php endforeach; ?>
+      </tbody>
+    </table>
+  <?php else : ?>
+    <p class="text-center">No Hay Usuarios Registrados</p>
+  <?php endif; ?>
+</div>
+
+<?php echo $paginacion ?>
